8e807020f54186bedcfb7311209204f9678aa8dc
Wisu Suntoyo <wisu@devel.web.id>
2019-06-12 16:04:07 +0700

    kancrit

--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -15,7 +15,6 @@ import Amplify from '@aws-amplify/core';
 import Auth from '@aws-amplify/auth';
 import CognitoConfig from '../../configs/cognito';
 
-
 Amplify.configure(CognitoConfig);
 
 const useStyles = makeStyles(theme => ({
